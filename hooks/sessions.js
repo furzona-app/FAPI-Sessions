e265a032d3ed55d1801996111244aca64ebe4749
furzona-app <furzona.master@gmail.com>
2026-02-08 15:17:00 +0100

    Initial commit

---
+++ hooks/sessions.js
@@ -0,0 +1,64 @@
+const crypto = require("crypto");
+
+module.exports = {
+  runContextHook({ context, db }) {
+    context.newSession = (userId) => {
+      const token = "ft2$" + crypto.randomBytes(48).toString('hex');
+      
+      db.run(`
+        INSERT INTO sessions (id, user_id, token) VALUES ($->new_id(), ?, ?)
+      `, userId, token);
+
+      return token;
+    };
+
+    context.isSessionValid = (token) => {
+      if (!token) {
+        return false;
+      }
+
+      const session = db.get(`
+        SELECT 1 FROM sessions WHERE token = ? LIMIT 1
+      `, token);
+
+      return session != undefined;
+    };
+
+    context.isLoggedIn = () => {
+      return context.thisUser != null;
+    };
+  },
+
+  refreshUser({ context, db, User }) {
+    const authHeader = context.req?.headers?.["authorization"];
+    context.thisUser = null;
+
+    if (authHeader) {
+      const session = db.get(`
+        SELECT user_id FROM sessions WHERE token = ? LIMIT 1
+      `, authHeader);
+
+      if (session != undefined) {
+        const user = db.get(`
+          SELECT ${User.ALL} FROM users WHERE id = ? LIMIT 1
+        `, session.user_id);
+
+        if (user != undefined) {
+          context.thisUser = user;
+        }
+      }
+    }
+  },
+
+  async runFunctionHook(name, body, { context, isLoggedIn, FunctionError }) {
+    if (!isLoggedIn()) {
+      throw new FunctionError("authRequired");
+    }
+
+    this.refreshUser(context);
+  },
+
+  async runRefreshHook({ context }) {
+    this.refreshUser(context);
+  }
+};
